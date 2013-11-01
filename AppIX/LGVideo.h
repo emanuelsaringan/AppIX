aae6bfffadae9c61968e0fb15c3ee5848acb0187
Emanuel Saringan <dev@lambdageek.com>
2013-11-01 15:47:34 +0800

    Added Khan academy videos, exercises, and physics trivia feature

---
+++ AppIX/LGVideo.h
@@ -0,0 +1,13 @@
+//
+//  LGVideo.h
+//  AppIX
+//
+//  Created by Emanuel Saringan on 11/1/13.
+//  Copyright (c) 2013 lambdageek. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LGVideo : NSObject
+
+@end
---
+++ AppIX/LGVideo.m
@@ -0,0 +1,13 @@
+//
+//  LGVideo.m
+//  AppIX
+//
+//  Created by Emanuel Saringan on 11/1/13.
+//  Copyright (c) 2013 lambdageek. All rights reserved.
+//
+
+#import "LGVideo.h"
+
+@implementation LGVideo
+
+@end
